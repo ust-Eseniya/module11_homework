02b736b5945cfb1f468a4e07e4bdde3469259d40
ust-Eseniya <162564390+ust-Eseniya@users.noreply.github.com>
2024-04-12 14:07:20 +0500

    Update module-11-1.js

--- module-11-1.js
+++ module-11-1.js
@@ -2,25 +2,17 @@ function parityCheck(arr){
 
 let oddNum = 0;
 let evenNum = 0;
-let NoNumb = 0;
 for (let i = 0; i < arr.length; i++) {
-  if (typeof arr[i] === 'number') {
-    if (arr[i] % 2 == 0) {
+  if (!isNaN(arr[i])) {
+    if (arr[i] % 2 === 0) {
       evenNum++;
-    }else if (arr [i] %2 !== 0){
+    } else {
       oddNum++;
     }
-  } else {
-    NoNumb++;
+   }
   }
-}
+ }
 
 const arr = [2, 5, 6, 0, 'noize', '25'];
-
-console.log ('total evem number: ' + evenNum);
-console.log ('total odd number: ' + oddNum);
-console.log ('Not are number: ' + NoNumb);
-
-}
 
 parityCheck (arr);
